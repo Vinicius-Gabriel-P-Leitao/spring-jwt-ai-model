d08afdb5ed92e34718db705ac6eb3725db1463f6
Vinícius Gabriel Pereira Leitão <vinicius.gabriel.p.leitao@gmail.com>
2025-08-09 23:27:47 -0300

    [feat]: Implementa primeira lógica simples de autenticação de usuário.

---
+++ src/main/java/com/model/domain/servlet/ServletInitializer.java
@@ -0,0 +1,12 @@
+package com.model.domain.servlet;
+
+import com.model.ModelApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ModelApplication.class);
+    }
+}
